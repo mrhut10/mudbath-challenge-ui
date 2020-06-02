92c09c226e1c3cb50f4c6e625af6d5245fd7a0c4
mrhut10 <mrhut10@protonmail.com>
2020-06-03 04:28:25 +1000

    refactor and justify center related products

--- app/components/productList.jsx
+++ app/components/productList.jsx
@@ -6,24 +6,17 @@ const keyMatch = keyname => objA => objB => obj[keyname] === key;
 const ProductList = ({products, exchangeRates, updateProductByKey}) => {
   const selectedCurrency = exchangeRates.selectedKey
   return (
-  <ul className="flex flex-wrap bg-green-200">
-    {products.map(product => {
-      const { id, name, description, relatedProducts, price } = product;
-      return (
-        <ProductListItem
-          className="sm:w-full md:w-1/2 lg:w-1/3 mb-4 p-2"
-          name={name}
-          key={id}
-          description={description}
-          relatedProducts={relatedProducts}
-          price={price}
-          selectedCurrency={selectedCurrency}
-          exchangeRates={exchangeRates}
-          allProducts={products}
-          updateProductByKey={updateProductByKey}
-        />
-      )
-    })}
+  <ul className="flex flex-wrap bg-green-200 justify-center">
+    {products.map(product => (
+      <ProductListItem
+        className="sm:w-full md:w-1/2 lg:w-1/3 mb-4 p-2"
+        id = {product.id}
+        key={product.id}
+        exchangeRates={exchangeRates}
+        allProducts={products}
+        updateProductByKey={updateProductByKey}
+      />
+    ))}
   </ul>
 )}
 
