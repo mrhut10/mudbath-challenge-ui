04f52bd314d29578868fc362f7be12d0ff5de071
mrhut10 <mrhut10@protonmail.com>
2020-06-03 04:27:46 +1000

    popup status hook

---
+++ app/hooks/usePopupState.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import useStack from './useStack'
+
+function usePopupState(){
+  const [AllValues, EndValue, addValue, removeValue, danerousEditValue] = useStack();
+  const openProductDetails = id => addValue({type: 'productDetail', id});
+  const openProductEdit = (id, user) => user === 'admin' ? addValue({type: 'productEdit', id}) : 'not loged in';
+  const openCurrency = () => addValue({type: 'currency'})
+  const closePopup = () => removeValue();
+  const danerousProductIDChange = (oldID, newID) => dangerSetValues(
+    AllValues.map(
+      value =>
+        value.type === 'productDetail' || value.type === 'productEdit' && value.id === oldID
+        ? {...value, id: newID}
+        : value
+    )
+  );
+  return {
+    openProductDetails,
+    openProductEdit,
+    openCurrency,
+    closePopup,
+    danerousProductIDChange,
+  }
+}
+
+export default usePopupState;
