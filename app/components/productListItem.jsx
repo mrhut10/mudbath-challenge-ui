12b5c11091546cc8f3bfc12af8f6b184d5e45d71
mrhut10 <mrhut10@protonmail.com>
2020-06-05 03:45:26 +1000

    removed unneed props and removed popups

--- app/components/productListItem.jsx
+++ app/components/productListItem.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Tile from './tile';
-import ProductDetails from './productDetailsPopup'
 import { findExchangeRate, findProductByID } from '../helpers'
 
-function ProductListItem ({id, exchangeRates, allProducts, updateProductByKey}) {
+function ProductListItem ({id, exchangeRates, allProducts}) {
   const selectedCurrency = exchangeRates.selectedKey;
-  const {name, description, price, relatedProducts} = findProductByID(id, allProducts);
-  const exRate = findExchangeRate(price.base ,exchangeRates.selectedKey, exchangeRates);
+  const {name, price} = findProductByID(id, allProducts);
+  const exRate = findExchangeRate(price.base, exchangeRates.selectedKey, exchangeRates);
 
   return (
     <li className="w-full sm:w-1/2 md:w-1/3 p-2">
@@ -16,12 +15,6 @@ function ProductListItem ({id, exchangeRates, allProducts, updateProductByKey}) 
           <li>
             price: {!exRate ? 'LOADING' :  `${(price.amount * exRate).toFixed(2)} (${selectedCurrency})`}
           </li>
-          <ProductDetails
-          id={id}
-          allProducts={allProducts}
-          exchangeRates={exchangeRates}
-          updateProductByKey={updateProductByKey}
-        />
         </ul>
       </Tile>
     </li>
