0d4f3dabb364931da5129770916baa6598b926b8
mrhut10 <mrhut10@protonmail.com>
2020-06-06 03:03:16 +1000

    added feature to remove multiple items from a stack

--- app/hooks/useStack.jsx
+++ app/hooks/useStack.jsx
@@ -5,9 +5,9 @@ function useStack(){
   const [allValues, setAllValues] = useState([]);
   const endValue = allValues.length > 0 ? allValues[allValues.length - 1] : undefined;
   const addValue = (newValue) => setAllValues([...allValues, newValue]);
-  const removeValue = () => allValues.slice(0, allValues.length - 1);
+  const removeValues = (count=1) => setAllValues(allValues.slice(0, allValues.length - count));
   const dangerSetValues = setAllValues;
-  return [allValues, endValue, addValue, removeValue, dangerSetValues];
+  return [allValues, endValue, addValue, removeValues, dangerSetValues];
 }
 
 export default useStack;
