673171b21a1f01d9c1a3d0bf687b95c7686dfa51
mrhut10 <mrhut10@protonmail.com>
2020-06-10 03:53:54 +1000

    tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 module.exports = {
   theme: {
-    colors: {
-      'mainbg': '#D3D9DC',
-      'cardbg': '#EBEBEB',
-      'mudbath-dark': '#082F44',
-      'mudbath-midium': '#39637A',
-      'mudbath-light': '#00BDFF',
-      'de-emph-gray': '#6A6A6A',
-    }
+    extend: {
+      colors: {
+        'mainbg': '#D3D9DC',
+        'cardbg': '#EBEBEB',
+        'buttonbg':'#E4ECF0',
+        'buttonbord':'#9ABFD2',
+        'linecolor': '#86A8BA',
+        'dark': '#082F44',
+        'medium': '#39637A',
+        'light': '#00BDFF',
+        'disabled': '#6A6A6A',
+        'deemphgrey':'#A2B4BE'
+      },
+      boxShadow: {
+        card: '0px 6px 5px rgba(0, 0, 0, 0.006), 0px 4px 4px rgba(0, 0, 0, 0.25)'
+      }
+    },
   }
 }
