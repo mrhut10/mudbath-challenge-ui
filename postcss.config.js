6861081f0811c7c62abdce5fee2984af8a44fc1f
mrhut10 <mrhut10@protonmail.com>
2020-06-10 00:58:14 +1000

    redid tailwindcss & postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,21 @@
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+const fullhuman = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production'
-      ? {
-          '@fullhuman/postcss-purgecss': {
-            content: [
-              './app/**/*.js'
-            ],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-          }
-        }
-      : {})
-  }
+  plugins : [
+    tailwindcss('./tailwind.config.js'),
+    autoprefixer,
+    ...(
+      process.env.NODE_ENV === 'production'
+      ? [
+        fullhuman({
+          content: [
+            './app/**/*.js'
+          ],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        })
+      ] : []
+    )
+  ],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    colors: {
+      'mainbg': '#D3D9DC',
+      'cardbg': '#EBEBEB',
+      'mudbath-dark': '#082F44',
+      'mudbath-midium': '#39637A',
+      'mudbath-light': '#00BDFF',
+      'de-emph-gray': '#6A6A6A',
+    }
+  }
+}
