ba77287fc6eaad4adfc561ca5c68ea0042b43196
mrhut10 <mrhut10@protonmail.com>
2020-06-12 23:13:05 +1000

    enabled tool tip option on button component

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,16 +1,40 @@
-import React, {ReactNode, MouseEventHandler} from 'react'
+import React, {ReactNode, MouseEventHandler, useState} from 'react'
+import Tooltip from './tooltip'
 
 interface ButtonProps {
   children: ReactNode,
   onClick?: MouseEventHandler<HTMLButtonElement>
   disabled?: boolean
   className?: string
+  tooltip?: ReactNode
 }
 
-const Button = ({children, onClick, disabled, className:CN}: ButtonProps) => (
-  <button disabled={disabled} className={`rounded-lg ${!disabled ? 'bg-buttonbg' : 'bg-disabled text-cardbg'} py-px px-2 border-2 border-buttonbord hover:border-light ` + CN} onClick={onClick}>
-    {children}
-  </button>
-)
+const Button = ({children, onClick, disabled, className:CN, tooltip}: ButtonProps) => {
+  
+  return (
+    tooltip
+    ? (
+      <Tooltip placement="bottom" trigger="hover" tooltip={tooltip} hideArrow={false}>
+        <button
+          className={`rounded-lg ${!disabled ? 'bg-buttonbg' : 'bg-disabled text-cardbg'} py-px px-2 border-2 border-buttonbord hover:border-light ` + CN}
+          onClick={onClick}
+          type="button"
+          disabled={disabled}
+        >
+          {children}
+        </button>
+      </Tooltip>
+    ) : (
+      <button
+        className={`rounded-lg ${!disabled ? 'bg-buttonbg' : 'bg-disabled text-cardbg'} py-px px-2 border-2 border-buttonbord hover:border-light ` + CN}
+        onClick={onClick}
+        type="button"
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    )
+  )
+}
 
 export default Button;
