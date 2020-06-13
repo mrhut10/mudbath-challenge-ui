3856dd5a7d265025c237c6060937d33be050b43e
mrhut10 <mrhut10@protonmail.com>
2020-06-14 03:29:23 +1000

    max width for cards on main page

--- app/components/productList.tsx
+++ app/components/productList.tsx
@@ -10,7 +10,7 @@ interface ProductListProps {
 
 const ProductList = ({children, selectedKeys=[], toogleChild=undefined}:ProductListProps) => {
   return (
-  <ul className="flex flex-wrap -ml-10 p-10 md:p-5">
+  <ul className="flex flex-wrap justify-center -ml-10 p-10 md:p-5">
     {children.map((child) => {
       const childKey = child.props.id
       const onClickHandler = !!toogleChild && typeof toogleChild === 'function'
@@ -18,7 +18,7 @@ const ProductList = ({children, selectedKeys=[], toogleChild=undefined}:ProductL
         : undefined
 
       return (
-        <div key={childKey} className="box-border w-full lg:w-1/2 pl-10 relative">
+        <div key={childKey} className="box-border w-full lg:w-1/2 max-w-2xl pl-10 relative">
           <div
             className="w-full"
             onClick={onClickHandler}
