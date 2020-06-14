58ee737bb9e0e2d14523b516783d9b1d0b80f6e8
mrhut10 <mrhut10@protonmail.com>
2020-06-15 02:53:59 +1000

    removed unneeded comments

--- app/components/productForm.tsx
+++ app/components/productForm.tsx
@@ -35,7 +35,7 @@ const failMessagesGenerator = (currencies:currencyStateInterface) => ({
 const ProductForm = ({id, allProducts, currencies,  saveProductByKey}:ProductFormProps) => {
   const failMessages = failMessagesGenerator(currencies)
   const product = findProductByID(id, allProducts)
-  const [fieldData, setFieldData] = useState<fieldsDescriptor>({
+  const resetData:fieldsDescriptor = {
     id: {
       validator: new TextValidator()
         .isNumber(failMessages.id)
@@ -74,8 +74,8 @@ const ProductForm = ({id, allProducts, currencies,  saveProductByKey}:ProductFor
         .isNumberMinValue(failMessages.priceAmount, 0),
       value: String(product.price.amount)
     },
-
-  })
+  }
+  const [fieldData, setFieldData] = useState<fieldsDescriptor>(resetData)
 
 
   const handleChange = (fieldName: string) => (e) => {
@@ -200,61 +200,6 @@ const ProductForm = ({id, allProducts, currencies,  saveProductByKey}:ProductFor
       </Button>
     </form>
   )
-  /*
-    return (
-    <form className="flex flex-wrap" onSubmit={handleSubmit}>
-      <label className="w-1/4 font-bold my-2" htmlFor="id">id:</label>
-      <input 
-        className={`w-3/4 bg-gray-400 my-2 ${invalidID ? 'bg-red-400':''}`}
-        id="id"
-        name="id"
-        type="number"
-        step="1"
-        required
-        min="1"
-        defaultValue={id}
-        onChange={(e)=>handlefieldChange('id')(e)}
-      />
-      {invalidID ? <span className="w-full text-red-400 text-right">ID already Used</span> : undefined}
-
-      <label className="w-1/4 font-bold my-2" htmlFor="name">name:</label>
-      <input className="w-3/4 bg-gray-400 my-2" id="name" name="name" type="text" required defaultValue={name}/>
-
-      <label className="w-1/4 font-bold my-2" htmlFor="description">description:</label>
-      <input className="w-3/4 bg-gray-400 my-2" id="description" name="description" type="text" required defaultValue={description}/>
-      
-      <label className="w-1/4 font-bold my-2" htmlFor="price">price (in {selectedCurrency}):</label>
-      <input className="w-1/4 bg-gray-400 my-2" id="price" name="price" type="number" step="0.01" required min="0.01" defaultValue={price.amount}/>
-
-      <span className="w-1/4 text-center my-2"> in </span>
-
-      <select 
-        name="currency"
-        id="currency"
-        defaultValue={price.base}
-        className="w-1/4 bg-gray-400 my-2"
-      >
-        {
-          (exchangeRates.status !== 'downloaded' ? [] : exchangeRates.data).map(
-            currency => <option key={currency.base} value={currency.base}>{currency.base}</option>
-          )
-        }
-      </select>
-      
-      <span className="w-full text-right">Calculates to {localCurrency} in {selectedCurrency}</span>
-
-      <label className="w-full text-center font-bold my-2" htmlFor="related">Related Products</label>
-      <ProductList
-        selectedKeys={trackedFields.relatedProducts}
-        toogleChild={toogleRelatedProduct}
-      >
-        {allProducts.map(product => (
-          <ProductListItem key={id} id={product.id} user={user} allProducts={allProducts} exchangeRates={exchangeRates} popupStack={popupStack} showDetailsButton={false} showEditButton={false}/>
-        ))}
-      </ProductList>
-      <button disabled={invalidID} className="mx-auto rounded-lg bg-red-200 p-2 my-2">Save Changes</button>
-    </form>
-  )*/
 }
 
 
