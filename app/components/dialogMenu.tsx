ef05e09c9b7775e97bbce5ec11f5e2581c99b94e
mrhut10 <mrhut10@protonmail.com>
2020-06-15 02:23:32 +1000

    title field in dialogMenu

--- app/components/dialogMenu.tsx
+++ app/components/dialogMenu.tsx
@@ -13,37 +13,39 @@ interface DialogMenuProps {
   user: users
   popupState: usePopupStateReturnInterface
   showEdit?: boolean
+  heading?: string
 }
 
-const DialogMenu = ({id, user, allProducts, popupState, showEdit=true}:DialogMenuProps) => (
+const DialogMenu = ({id, user, allProducts, popupState, showEdit=true, heading}:DialogMenuProps) => (
   <div className="flex flex-col justify-between">
+    {/* Title and Menu Buttons */}
     <div className="flex justify-between">
-      <h2 className="m-2 text-xl">{name}</h2>
-        <div className="space-x-3 flex">
-          {
-            showEdit &&
-            <Button
-              onClick={()=>user === 'admin' && popupState.openProductEdit(allProducts, id, user)}
-              className={user === 'admin' ? 'my-2' :'my-2 bg-disabled'}
-              tooltip={user === 'admin' ? undefined : 'Login To Enable Editing'}
-              disabled={user === 'user'}
-            >
-              Edit
-            </Button>
-          }
-          
-            <Tooltip placement="bottom" tooltip="close" trigger="hover" hideArrow={false}>
-              <button
-                className="p-2 text-xl text-red-400 bg-transparent "
-                onClick={()=>popupState.closePopups(1)}
-              >
-                <span>X</span>
-                <span className="sr-only">close</span>
-              </button>
-            </Tooltip>
-          </div>
-        </div>
-        <div className="m-2 w-full flex flex-wrap">
+      <h2 className="m-2 text-xl">{heading}</h2>
+      <div className="space-x-3 flex">
+        {
+          showEdit &&
+          <Button
+            onClick={()=>user === 'admin' && popupState.openProductEdit(allProducts, id, user)}
+            className={user === 'admin' ? 'my-2' :'my-2 bg-disabled'}
+            tooltip={user === 'admin' ? undefined : 'Login To Enable Editing'}
+            disabled={user === 'user'}
+          >
+            Edit
+          </Button>
+        }
+        <Tooltip placement="bottom" tooltip="close" trigger="hover" hideArrow={false}>
+          <button
+            className="p-2 text-xl text-red-400 bg-transparent "
+            onClick={()=>popupState.closePopups(1)}
+          >
+            <span>X</span>
+            <span className="sr-only">close</span>
+          </button>
+        </Tooltip>
+      </div>
+    </div>
+    {/* Breadcrumbs Nav */}
+    <div className="m-2 w-full flex flex-wrap">
           {
             popupState.wholeStack
             // not include last value & trim enough from front to keep under max number
@@ -66,7 +68,7 @@ const DialogMenu = ({id, user, allProducts, popupState, showEdit=true}:DialogMen
             })
           }
         </div>
-      </div>
+    </div>
 )
 
 export default DialogMenu
