fb2c4bd8b45e7318b0fb63dc660cd8bef8c0f633
mrhut10 <mrhut10@protonmail.com>
2020-06-15 02:22:04 +1000

    make input component

---
+++ app/components/Input.tsx
@@ -0,0 +1,70 @@
+import React, { ButtonHTMLAttributes, InputHTMLAttributes, ChangeEvent } from 'react'
+
+interface InputProps {
+  id: string,
+  type: InputHTMLAttributes<HTMLInputElement>["type"]
+  validatorResult: [boolean, string] | boolean
+  className?: string
+  onChange: (e:ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => void
+  // remainding options
+  [key:string]: any
+}
+const Input = ({id, type, validatorResult, onChange, className:CN, options, ...extraProps}:InputProps) => {
+
+  // if textArea
+  if (type === 'textArea'){
+    return (
+      <textarea
+        id={id}
+        name={id}
+        onChange={onChange}
+        className={
+          (validatorResult[0] ? 'bg-white' : 'bg-red-200') 
+          + ' rounded-lg p-2 '
+          + CN
+        }
+        {...extraProps}
+      />
+    )
+  }
+
+  if (type === 'select'){
+    return (
+      <select 
+        id={id}
+        name={id}
+        className={
+          (validatorResult[0] ? 'bg-buttonbg' : 'bg-red-200') 
+          + ' rounded-l-lg -p-2 appearance-none -m-2'
+          + CN
+        }
+        onChange={onChange}
+        {...extraProps}>
+        {
+          Array.isArray(options) && options.map(item => <option key={item} value={item}>{item}</option>)
+        }
+        <option value='invalid'>invalid</option>
+      </select>
+    )
+  }
+  
+  // default input
+  return (
+    <input
+      id={id}
+      name={id}
+      type={type}
+      onChange={onChange}
+      className={
+        (validatorResult[0] ? 'bg-white' : 'bg-red-200') 
+        + ' rounded-lg p-2 '
+        + CN
+      }
+      {...extraProps}
+    />
+  )
+
+}
+
+
+export default Input
