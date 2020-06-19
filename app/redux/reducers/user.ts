26cef17dca6932b6311d42195283f9d224ee355d
mrhut10 <mrhut10@protonmail.com>
2020-06-20 03:20:27 +1000

    user reducer

---
+++ app/redux/reducers/user.ts
@@ -0,0 +1,12 @@
+import {USER_LOGIN, ACTION_USER_LOGIN} from '../actionTypes'
+import { bindActionCreators } from 'redux'
+
+export type userNames = 'user' | 'admin'
+
+const initialState: userNames = 'user'
+
+export default function(state: userNames=initialState, action: ACTION_USER_LOGIN) {
+  if (action.type === USER_LOGIN && typeof action.payload === 'string' && ['user', 'admin'].includes(action.payload)){
+    return action.payload
+  }
+}
